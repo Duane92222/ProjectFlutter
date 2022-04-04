3cec6917fda05488513670a965642438e47052ba
duane92222 <duane.estrade@laposte.net>
2022-04-04 20:05:46 +0200

    hi

---
+++ app_clutter_chat/lib/HomePage/FirestoreHelper.dart
@@ -0,0 +1,62 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+
+class FirestoreHelper {
+  //Attributs
+  final auth = FirebaseAuth.instance;
+  final fire_user = FirebaseFirestore.instance.collection("Utilisateurs");
+
+
+
+  //Constructeur
+
+
+
+  //méthode
+
+//Pour l'inscription
+Future Incription(String mail,String password,String nom, String prenom) async {
+  UserCredential resultat = await auth.createUserWithEmailAndPassword(email: mail, password: password);
+ User? user = resultat.user;
+ String uid = user!.uid;
+ Map<String,dynamic>map = {
+   "NOM":nom,
+   "PRENOM": prenom,
+   "MAIL": mail
+ };
+ addUser(uid, map);
+
+}
+
+
+
+//Pour la connexion
+
+Future Connexion(String mail, String password) async {
+  UserCredential resultat = await auth.signInWithEmailAndPassword(email: mail, password: password);
+
+}
+
+
+//Ajouter des utilisateurs
+addUser(String uid,Map<String,dynamic>map){
+  fire_user.doc(uid).set(map);
+
+}
+
+
+//Modifier les informations d'un utilisateur
+  updatedUser(String uid,Map<String,dynamic>map){
+    fire_user.doc(uid).update(map);
+
+  }
+
+
+
+
+
+}
